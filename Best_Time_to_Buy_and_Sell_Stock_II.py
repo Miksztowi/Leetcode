bfb50bef06765f772b086b53eb1168ce703d84d5
Wukong <binwengan@gmail.com>
2017-10-22 23:51:35 +0800

    DP formula: sold = max(sold, bought + cur_price - fee) if sell stock is the best choice or continue holding the stock. bought = max(bought, sold - cur_price)  Buy the new stock if after that the money still more than begin.

---
+++ Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -0,0 +1,27 @@
+# -*- encoding:utf-8 -*-
+# __author__=='Gan'
+
+# Say you have an array for which the ith element is the price of a given stock on day i.
+# Design an algorithm to find the maximum profit. You may complete as many transactions as you like
+# (ie, buy one and sell one share of the stock multiple times).
+# However, you may not engage in multiple transactions at the same time
+# (ie, you must sell the stock before you buy again).
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+        sold, bought = 0, float('-inf')
+        for p in prices:
+            sold = max(sold, bought + p)
+            bought = max(bought, sold - p)
+        return sold if sold > 0 else 0
+
+
+if __name__ == '__main__':
+    print(Solution().maxProfit([1, 3, 2, 8, 4, 9]))
+    print(Solution().maxProfit([1, 3, 7, 5, 10, 3]))
