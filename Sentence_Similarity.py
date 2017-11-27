19caf610c11752ccc03b690a25db1f8d76ac5c4c
Wukong <binwengan@gmail.com>
2017-11-28 00:42:17 +0800

    LeetCode Contest 60.

--- Sentence_Similarity.py
+++ Sentence_Similarity.py
@@ -11,7 +11,10 @@
 # Also, a word is always similar with itself. For example, the sentences words1 = ["great"], words2 = ["great"],
 # pairs = [] are similar, even though there are no specified similar word pairs.
 
-
+# LeetCode Contest 60.
+# 49 / 49 test cases passed.
+# Status: Accepted
+# Runtime: 32 ms
 import collections
 class Solution(object):
     def areSentencesSimilar(self, words1, words2, pairs):
@@ -27,7 +30,6 @@ class Solution(object):
         for word1, word2 in pairs:
             words[word1].add(word2)
             words[word2].add(word1)
-
         for word1, word2 in zip(words1, words2):
             if word1 != word2 and word2 not in words[word1]:
                 return False
@@ -42,19 +44,17 @@ if __name__ == '__main__':
                                          ['fine', 'drama', 'talent'],
                                          [["great", "fine"], ["acting", "drama"], ["skills", "talent"]]))
 
-    print(Solution().areSentencesSimilar(["an", "extraordinary", "meal"],
-                                         ["a", "good", "dinner"],
-                                         [["great", "good"], ["extraordinary", "good"], ["well", "good"],
-                                          ["wonderful", "good"], ["excellent", "good"], ["fine", "good"],
-                                          ["nice", "good"], ["any", "one"], ["some", "one"], ["unique", "one"],
-                                          ["the", "one"], ["an", "one"], ["single", "one"], ["a", "one"],
-                                          ["truck", "car"], ["wagon", "car"], ["automobile", "car"], ["auto", "car"],
-                                          ["vehicle", "car"], ["entertain", "have"], ["drink", "have"], ["eat", "have"],
-                                          ["take", "have"], ["fruits", "meal"], ["brunch", "meal"],
-                                          ["breakfast", "meal"], ["food", "meal"], ["dinner", "meal"],
-                                          ["super", "meal"], ["lunch", "meal"], ["possess", "own"], ["keep", "own"],
-                                          ["have", "own"], ["extremely", "very"], ["actually", "very"],
-                                          ["really", "very"], ["super", "very"]]))
-    # print(Solution().areSentencesSimilar(['g'],
-    #                                      ['g'],
-    #                                      []))
+
+    # print(Solution().areSentencesSimilar(["an", "extraordinary", "meal"],
+    #                                      ["a", "good", "dinner"],
+    #                                      [["great", "good"], ["extraordinary", "good"], ["well", "good"],
+    #                                       ["wonderful", "good"], ["excellent", "good"], ["fine", "good"],
+    #                                       ["nice", "good"], ["any", "one"], ["some", "one"], ["unique", "one"],
+    #                                       ["the", "one"], ["an", "one"], ["single", "one"], ["a", "one"],
+    #                                       ["truck", "car"], ["wagon", "car"], ["automobile", "car"], ["auto", "car"],
+    #                                       ["vehicle", "car"], ["entertain", "have"], ["drink", "have"], ["eat", "have"],
+    #                                       ["take", "have"], ["fruits", "meal"], ["brunch", "meal"],
+    #                                       ["breakfast", "meal"], ["food", "meal"], ["dinner", "meal"],
+    #                                       ["super", "meal"], ["lunch", "meal"], ["possess", "own"], ["keep", "own"],
+    #                                       ["have", "own"], ["extremely", "very"], ["actually", "very"],
+    #                                       ["really", "very"], ["super", "very"]]))
