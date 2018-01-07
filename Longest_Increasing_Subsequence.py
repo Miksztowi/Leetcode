af2298562e4a79b3ac8c7217b4802c173a8368b5
Wukong <binwengan@gmail.com>
2018-01-07 10:19:19 +0800

    Add Binary Search solution.

--- Longest_Increasing_Subsequence.py
+++ Longest_Increasing_Subsequence.py
@@ -32,6 +32,24 @@ class Solution(object):
                     dp[i] = max(dp[j] + 1, dp[i])
         return max(dp)
 
+# Binary Search.
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        tails = [0] * len(nums)
+        size = 0
+        for x in nums:
+            i, j = 0, size
+            while i != j:
+                m = (i + j) // 2
+                if tails[m] < x:
+                    i = m + 1
+                else:
+                    j = m
+            tails[i] = x
+            size = max(i + 1, size)
+        return size
+
+# Runtime: 48 ms
 if __name__ == '__main__':
     print(Solution().lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
     print(Solution().lengthOfLIS([10,9,2,5,3,4]))
